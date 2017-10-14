0cb4c4acf03bb03134ca4ed1e07c959712f892ab
eyal.kopelevich <eyal.kopelevich@EYALKOPEL-LT>
2017-10-14 09:52:02 +0300

    new commit

--- src/p1/testappium.java
+++ src/p1/testappium.java
@@ -38,7 +38,7 @@ public class testappium implements Runnable {
 		//  dc.setCapability("projectName", "Default");
 		//  dc.setCapability(MobileCapabilityType.UDID, "bb5f832e3ee56f952af3f8ae1983b4de9e4b0951");
 		
-		dc.setCapability(MobileCapabilityType.APP, "http://192.168.2.72:8181/AndroidApps/eribank.apk");
+		dc.setCapability(MobileCapabilityType.APP, "C:\\Users\\eyal.kopelevich\\Downloads\\EriBank.apk");
 		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
 		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
 		dc.setCapability("instrumentApp", true);
